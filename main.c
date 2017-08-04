590e1436955803c0c367cc1a740a0c1de0d0b7a0
Karanraj Chauhan <chauhank@bu.edu>
2017-08-04 17:59:21 -0400

    fixed define bugs

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <msp430.h> 
 
 // Defines for LED
-#define LED_PORT_ P1
+#define LED_PORT P1
 #define LED_PIN BIT5
 
 // Defines for trigger pin and pulse
-#define TRIG_PORT_ P1
+#define TRIG_PORT P1
 #define TRIG_PIN BIT2
 #define TRIG_LOW_TIME 25000-1
 #define TRIG_HIGH_TIME 100
 
 // Defines for echo pin and pulse
-#define ECHO_PORT_ P2
+#define ECHO_PORT P2
 #define ECHO_PIN BIT2
 #define MAX_ECHO_HIGH_TIME 23500
 
@@ -25,8 +25,8 @@
  *************************************************************************/
 void set_up_led_output(void)
 {
-    LED_PORT_DIR |= LED_PIN;        // set as output pin
-    LED_PORT_OUT &= ~LED_PIN;       // set output low on pin
+    P1DIR |= LED_PIN;        // set as output pin
+    P1OUT &= ~LED_PIN;       // set output low on pin
 }
 
 
@@ -39,8 +39,8 @@ void set_up_led_output(void)
  *************************************************************************/
 void set_up_trig_pulse(void)
 {
-    TRIG_PORT_DIR |= TRIG_PIN;          // P1.2 to output
-    TRIG_PORT_SEL |= TRIG_PIN;          // P1.2 to TA0.1
+    P1DIR |= TRIG_PIN;          // P1.2 to output
+    P1SEL |= TRIG_PIN;          // P1.2 to TA0.1
 
     TA0CTL = TASSEL_2                   // timer source SMCLK
             + MC_1;                     // up mode to CCR0
@@ -61,15 +61,15 @@ void set_up_trig_pulse(void)
 void set_up_read_echo(void)
 {
     // set up echo pin to be input pin with pull down resistor
-    ECHO_PORT_SEL &= ~ECHO_PIN;     // i/o function selected, 0
-    ECHO_PORT_DIR &= ~ECHO_PIN;     // input mode (p3.5), 0
-    ECHO_PORT_OUT &= ~ECHO_PIN;     // select pull-down mode, 0
-    ECHO_PORT_REN |= ECHO_PIN;      // enable internal pull up, 1
+    P2SEL &= ~ECHO_PIN;     // i/o function selected, 0
+    P2DIR &= ~ECHO_PIN;     // input mode (p3.5), 0
+    P2OUT &= ~ECHO_PIN;     // select pull-down mode, 0
+    P2REN |= ECHO_PIN;      // enable internal pull up, 1
 
     // set up interrupt on echo pin reading low to high
-    ECHO_PORT_IE |= ECHO_PIN;       // enable interrupt for echo pin
-    ECHO_PORT_IES &= ~ECHO_PIN;     // low to high transition
-    ECHO_PORT_IFG &= ~ ECHO_PIN     // clear the interrupt flag
+    P2IE |= ECHO_PIN;       // enable interrupt for echo pin
+    P2IES &= ~ECHO_PIN;     // low to high transition
+    P2IFG &= ~ ECHO_PIN;    // clear the interrupt flag
 }
 
 
@@ -79,11 +79,11 @@ void set_up_read_echo(void)
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    //ECHO_PORT_IE &= ~ECHO_PIN;      // disable interrupt don't interrupt if already inside interrupt
+    //P2IE &= ~ECHO_PIN;      // disable interrupt don't interrupt if already inside interrupt
 
     // count how long the echo input pin stays high
     unsigned long long highTimeCount = 0;
-    while ((ECHO_PORT_IN & ECHO_PIN) != 0)
+    while ((P2IN & ECHO_PIN) != 0)
     {
         highTimeCount++;
     }
@@ -91,11 +91,11 @@ __interrupt void Port_2(void)
     // if it stayed high for anything less than the max value, turn on LED. Otherwise, turn it off
     if (highTimeCount < MAX_ECHO_HIGH_TIME)
     {
-        LED_PORT_OUT |= LED_PIN;
+        P1OUT |= LED_PIN;
     }
     else
     {
-        LED_PORT_OUT &= ~LED_PIN;
+        P1OUT &= ~LED_PIN;
     }
 }
 
