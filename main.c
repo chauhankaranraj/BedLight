e0d60980f7613ce4ff89687e0c5dc49e72b747ed
Karan <chauhank@bu.edu>
2017-08-04 15:22:31 -0400

    added defines. started echo pulse reading function

--- main.c
+++ main.c
@@ -1,29 +1,57 @@
 #include <msp430.h> 
 
+// Defines for trigger pin and pulse
+#define TRIG_PORT_ P1
+#define TRIG_PIN BIT2
+#define TRIG_LOW_TIME 2500-1
+#define TRIG_HIGH_TIME 100
 
-/*********************************************************************
+
+// TODO: find the correct port and pin to be associated
+// Defines for echo pin and pulse
+#define ECHO_PORT_ P2
+#define ECHO_PIN BIT2
+#define ECHO_LOW_TIME 2500-1
+#define ECHO_HIGH_TIME 100
+
+
+/*************************************************************************
  * Set up output from timer on set/reset mode
  * Set every 2400us, reset in 100us after set
- *********************************************************************/
+ *
+ * PWM will stay low for TRIG_LOW_TIME, then go high for TRIG_HIGH_TIME,
+ * and then low again (duty cycle = ~3.85%)
+ *************************************************************************/
 void set_up_trig_pulse(void)
 {
-    P2DIR = 0;              // set port 2 to inputs
+    TRIG_PORT_DIR |= TRIG_PIN;          // P1.2 to output
+    TRIG_PORT_SEL |= TRIG_PIN;          // P1.2 to TA0.1
 
-    P1DIR |= BIT2;          // P1.2 to output
-    P1SEL |= BIT2;          // P1.2 to TA0.1
+    TA0CTL = TASSEL_2                   // timer source SMCLK
+            + MC_1;                     // up mode to CCR0
 
-    TA0CTL = TASSEL_2       // timer source SMCLK
-            + MC_1;         // up mode to CCR0
-
-    TA0CCTL0 = OUTMOD_3;    // set/reset mode
-    TA0CCR0 = 2500-1;       // will set output low after 100us of high
-    TA0CCR1 = 100;          // will set output high when 100 have passed since low
+    TA0CCTL0 = OUTMOD_3;                // set/reset mode
+    TA0CCR0 = TRIG_LOW_TIME;            // will set output low after 100us of high
+    TA0CCR1 = TRIG_HIGH_TIME;           // will set output high when 100 have passed since low
 }
 
 
-/*********************************************************************
+/*************************************************************************
+ * Set up output from timer on set/reset mode
+ * Set every 2400us, reset in 100us after set
+ *
+ * PWM will stay low for TRIG_LOW_TIME, then go high for TRIG_HIGH_TIME,
+ * and then low again (duty cycle = ~3.85%)
+ *************************************************************************/
+void set_up_read_echo(void)
+{
+
+}
+
+
+/*************************************************************************
  * main.c
- *********************************************************************/
+ *************************************************************************/
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
